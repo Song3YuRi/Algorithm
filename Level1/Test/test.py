70486331b47da5c1ed860efd06002b8fb72bc0e1
Song3YuRi <dbfldi10@swu.ac.kr>
2022-01-16 09:51:26 +0900

    test.py

--- Level1/Test/test.py
+++ Level1/Test/test.py
@@ -1,3 +1,70 @@
 def solution(answers):
     answer = []
+    
+    student = [0,0,0]
+    for i in range(3):
+        
+        person_answer = i+1
+        
+        count = 1
+        for a in answers:            
+            if i == 0:
+                if a == person_answer:
+                    studnet[i] += 1
+                    person_answer += 1
+                else:
+                    person_answer += 1
+                
+                if person_answer == 6:
+                    person_answer = 1
+            elif i == 1:
+                if count % 2 != 0:
+                    if person_answer == 2:
+                        person_answer = 1
+                        
+                    if a == person_answer:
+                        studnet[i] += 1
+                    
+                    if person_answer == 1:
+                        person_answer += 2
+                    else:
+                        person_answer += 1
+                    
+                    if person_answer > 5:
+                        person_answer = 1
+                    
+                    count += 1
+                else:
+                    if a == 2:
+                        student[i] += 1
+                    count += 1
+            elif i == 2:
+                
+                if person_answer == a:
+                    studnet[i] += 1
+                    if count == 1:
+                        count += 1
+                    elif count == 2:
+                        count = 1
+                        if person_answer == 3:
+                            person_answer = 1
+                        else:
+                            person_answer += 1
+                        
+                        if person_answer > 5:
+                            person_answer = 3
+                else:
+                    if count == 1:
+                        count += 1
+                    elif count == 2:
+                        count = 1
+                        if person_answer == 3:
+                            person_answer = 1
+                        else:
+                            person_answer += 1
+                        
+                        if person_answer > 5:
+                            person_answer = 3    
+    
+    answer.sort()
     return answer
