adcc1d62d799593eb1dbd853a89fdca4e7aafdad
Song3YuRi <91586461+Song3YuRi@users.noreply.github.com>
2022-01-29 20:52:35 +0900

    Create solution.py

---
+++ Level1/가운데 글 가져오기/solution.py
@@ -0,0 +1,12 @@
+
+def solution(s):
+    answer = ''
+    
+    if len(s) % 2 == 0:
+        idx = len(s) // 2
+        answer = s[idx - 1] + s[idx]
+    else:
+        idx = (len(s) - 1) // 2
+        answer = s[idx]
+    
+    return answer
