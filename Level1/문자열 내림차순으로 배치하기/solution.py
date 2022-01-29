cd2f58c9247e5c5c53c81c91494053f16d70eb50
Song3YuRi <91586461+Song3YuRi@users.noreply.github.com>
2022-01-29 23:22:30 +0900

    Update solution.py

--- Level1/문자열 내림차순으로 배치하기/solution.py
+++ Level1/문자열 내림차순으로 배치하기/solution.py
@@ -9,20 +9,10 @@ def solution(s):
             Up += i
         else:
             Lower += i
+
+    s1 = ''.join(sorted(Lower, reverse=True))
+    s2 = ''.join(sorted(Up, reverse=True))
+
+    answer = s1 + s2
     
-    for i in range(len(Lower) - 1, 0, -1):
-        for j in range(0, i):
-            if Lower[j] < Lower[j+1]:
-                tmp = Lower[j]
-                Lower[j] = Lower[j+1]
-                Lower[j+1] = tmp
-    
-    for i in range(len(Up) - 1, 0, -1):
-        for j in range(0, i):
-            if Up[j] < Up[j+1]:
-                tmp = Up[j]
-                Up[j] = Up[j+1]
-                Up[j+1] = tmp
-    
-    answer = Lower + Up
     return answer
