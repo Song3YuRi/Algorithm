b57ffd1ed4bfcac5972c54caa8fc44f68de31ac7
Song3YuRi <91586461+Song3YuRi@users.noreply.github.com>
2022-02-12 16:51:43 +0900

    Update solution.py

--- Level1/실패율/solution.py
+++ Level1/실패율/solution.py
@@ -1,36 +1,15 @@
 def solution(N, stages):
     answer = []
 
-    stage_rate = {}
-    sort_list = []
-    sort_key = []
+    failure = {}
 
+    total = len(stages)
     for i in range(1, N+1):
-        
-        total_count = 0
-        count = 0
-        for s in stages:
-            if i == s or i < s:
-                total_count += 1
-                if i == s:
-                    count += 1
-
-        failure_rate = count / total_count 
-        stage_rate[i] = failure_rate
-        sort_list.append(failure_rate)
-        sort_key.append(failure_rate)
-
-    sort = stage_rate.keys()
-
-    sort_list.sort(reverse=True)
+        if total != 0:
+            count = stages.count(i)
+            failure[i] = count / total
+            total -= count
+        else:
+            failure[i] = 0
     
-    for i in sort_list:
-        for key, value in stage_rate.items():
-            if value == i:
-                answer.append(key)
-                del stage_rate[key]
-                break
-
-    print("answer : ", answer)
-    print("sort_list : ", sort_list)
-    return answer
+    return sorted(failure, key=lambda x : failure[x], reverse=True)
